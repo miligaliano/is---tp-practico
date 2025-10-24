09fa0255a91432721013667465ed2827824596ea
miligaliano <miligaliano@gmail.com>
2025-10-24 14:06:13 -0300

    correcciones tests

--- test_comprar_entradas.py
+++ test_comprar_entradas.py
@@ -23,7 +23,7 @@ def usuario_no_registrado():
 
 def test_compra_exitosa_tarjeta(usuario_registrado):
     """Compra válida con tarjeta (Mercado Pago)"""
-    fecha = date.today() + timedelta(days=2)  # Una fecha futura válida
+    fecha = date.today() + timedelta(days=3)  # Una fecha futura válida
     compra = Compra(usuario_registrado, fecha, 2, [
                     25, 30], "regular", "tarjeta")
     resultado = compra.procesar()
@@ -33,7 +33,7 @@ def test_compra_exitosa_tarjeta(usuario_registrado):
 
 def test_compra_sin_forma_pago(usuario_registrado):
     """Falla sin forma de pago"""
-    fecha = date.today() + timedelta(days=2)
+    fecha = date.today() + timedelta(days=3)
     compra = Compra(usuario_registrado, fecha, 2, [25, 30], "VIP", None)
     resultado = compra.procesar()
     assert resultado["ok"] is False
@@ -90,7 +90,7 @@ def test_compra_sin_edades(usuario_registrado):
 
 def test_compra_pago_efectivo(usuario_registrado):
     """Pago en boletería válido"""
-    fecha = date.today() + timedelta(days=2)
+    fecha = date.today() + timedelta(days=3)
     compra = Compra(usuario_registrado, fecha, 2, [
                     18, 22], "regular", "efectivo")
     resultado = compra.procesar()
@@ -100,7 +100,7 @@ def test_compra_pago_efectivo(usuario_registrado):
 
 def test_compra_tipo_vip(usuario_registrado):
     """Pase VIP válido"""
-    fecha = date.today() + timedelta(days=2)
+    fecha = date.today() + timedelta(days=3)
     compra = Compra(usuario_registrado, fecha, 2, [18, 22], "VIP", "tarjeta")
     resultado = compra.procesar()
     monto_esperado = 2 * 15000  # 2 entradas VIP
@@ -153,7 +153,7 @@ def test_compra_fecha_pasada_falla(usuario_registrado):
 
 def test_compra_cantidad_limite_inferior_valida(usuario_registrado):
     """Prueba que se pueda comprar exactamente 1 entrada."""
-    fecha = date.today() + timedelta(days=2)
+    fecha = date.today() + timedelta(days=3)
     compra = Compra(usuario_registrado, fecha, 1, [40], "VIP", "tarjeta")
     resultado = compra.procesar()
     assert resultado["ok"] is True
@@ -161,7 +161,7 @@ def test_compra_cantidad_limite_inferior_valida(usuario_registrado):
 
 def test_compra_cantidad_limite_superior_valida(usuario_registrado):
     """Prueba que se puedan comprar exactamente 10 entradas."""
-    fecha = date.today() + timedelta(days=2)
+    fecha = date.today() + timedelta(days=3)
     edades = [25] * 10
     compra = Compra(usuario_registrado, fecha, 10,
                     edades, "regular", "efectivo")
@@ -171,7 +171,7 @@ def test_compra_cantidad_limite_superior_valida(usuario_registrado):
 
 def test_compra_cantidad_cero_falla(usuario_registrado):
     """Prueba que no se puedan comprar 0 entradas."""
-    fecha = date.today() + timedelta(days=2)
+    fecha = date.today() + timedelta(days=3)
     compra = Compra(usuario_registrado, fecha, 0, [], "regular", "tarjeta")
     resultado = compra.procesar()
     assert resultado["ok"] is False
